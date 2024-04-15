45ed4dc1359c6d0872d2048112a2dce394ffc27f
Dipayan Das <105163490+dasdipayan2002@users.noreply.github.com>
2024-04-15 10:43:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+  if (str.trim() === "") {
+    return "";
+  }
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      return str[i];
+    }
+  }
 }
 
 // Do not change the code below
